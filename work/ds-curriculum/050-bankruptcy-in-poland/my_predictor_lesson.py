301dd125e842d593e395382322d0b622f95ee7be
valdoth <valdotsiarohasina@gmail.com>
2022-07-16 16:29:52 +0300

    work

---
+++ work/ds-curriculum/050-bankruptcy-in-poland/my_predictor_lesson.py
@@ -0,0 +1,18 @@
+# Import libraries
+import gzip
+import json
+import pickle
+
+import pandas as pd
+
+
+# Add wrangle function from lesson 5.4
+def wrangle(filename):
+    
+    return df
+
+
+# Add make_predictions function from lesson 5.3
+def make_predictions(data_filepath, model_filepath):
+    
+    return y_test_pred
